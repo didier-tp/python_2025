28e26f3021140cfcf91af9cf49717f63088d5c13
Didier_Defrance <didier@d-defrance.fr>
2025-06-30 12:23:17 +0200

    s

--- solutions_tp_juillet2025/a7.py
+++ solutions_tp_juillet2025/a7.py
@@ -3,3 +3,9 @@ for l in range(8):
     for c in range(l+1):
         ligne_etoiles+="*"
     print(ligne_etoiles)
+
+# ou bien , plus simplement
+ligne_etoiles=""
+for l in range(8):
+    ligne_etoiles += "*" # ligne_etoiles = ligne_etoiles + "*"
+    print(ligne_etoiles)
