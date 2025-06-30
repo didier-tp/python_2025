bdcb1a959a442ecaa2f66746fcb10c83610dda49
Didier_Defrance <didier@d-defrance.fr>
2025-06-30 15:47:55 +0200

    s

---
+++ solutions_tp_juillet2025/b4Bis.py
@@ -0,0 +1,13 @@
+couleurs_en_fr = { 'red' : 'rouge',
+                   'blue' : 'blue'}
+couleurs_en_fr['green']='vert'
+
+print("traduction de red=", couleurs_en_fr['red'])
+print("traduction de green=", couleurs_en_fr['green'])
+
+dico_pers = { 'prenom': 'jean' , 'nom' : 'Bon' , 'age' : 33}
+dico_pers['taille']=172 #taille en cm
+print("age de la personne=", dico_pers['age'])
+dico_pers['age']+=1
+print("nouvel age de la personne=", dico_pers['age'])
+print("dico_pers=", dico_pers)
