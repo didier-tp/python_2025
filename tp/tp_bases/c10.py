5b1cb5e9f833fb3bddbe61c0f434404cc2936535
Didier_Defrance <didier@d-defrance.fr>
2025-07-01 16:54:13 +0200

    s

--- tp/tp_bases/c10.py
+++ tp/tp_bases/c10.py
@@ -1,3 +1,5 @@
+import functools
+
 liste_produits = [
 { 'id' : 1 , 'label' : 'cahier' , 'price' : 2.4 },
 { 'id' : 11 , 'label' : 'classeur' , 'price' : 4.4 },
@@ -9,5 +11,8 @@ liste_produits = [
 liste_produits_pas_chers = list(filter(lambda p: p['price'] <= 4, liste_produits))
 print("liste_produits_pas_chers=", liste_produits_pas_chers)
 
-liste_prix_produits_pas_chers = list(map(lambda ...., liste_produits_pas_chers))
+liste_prix_produits_pas_chers = list(map(lambda p :p['price'], liste_produits_pas_chers))
 print("liste_prix_produits_pas_chers=", liste_prix_produits_pas_chers)
+
+prix_total = functools.reduce(lambda x, y: x + y, liste_prix_produits_pas_chers)
+print(f'prix_total= {prix_total}') # 6.21
