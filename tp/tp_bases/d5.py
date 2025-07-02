a284449857b96f65666d5051046958c0a6cc7d62
Didier_Defrance <didier@d-defrance.fr>
2025-07-02 14:40:59 +0200

    s

--- tp/tp_bases/d5.py
+++ tp/tp_bases/d5.py
@@ -1,6 +1,15 @@
-with open("d5inputs.txt","rt") as finput :
+import math
+
+with open("d5inputs.txt","rt") as finput , open("d5outputs.txt","wt") as foutput:
     for ligneLue in finput :
-        if ligneLue.endswith("\n") :
-            ligneLue=ligneLue[:-1]
-        print(ligneLue)
+        try:
+            if ligneLue.endswith("\n") :
+                ligneLue=ligneLue[:-1]
+            vn=int(ligneLue)
+            racine=math.sqrt(vn)
+            foutput.write(f"{racine}\n")
+        except Exception as e:
+            print("execption:",e)
     #automatic call of .__exit__() et .close() à la fin du bloc with
+
+print("suite et fin de d5")    
