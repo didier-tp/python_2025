a91779d40254933c786d9ee05f13482003556122
Didier_Defrance <didier@d-defrance.fr>
2025-07-03 11:50:52 +0200

    s

--- tp/tp_bases/e4_Employe.py
+++ tp/tp_bases/e4_Employe.py
@@ -8,3 +8,14 @@ class Employe(Personne):
 
     def __str__(self):
         return f"Employe salaire={self.salaire} fonction={self.fonction} heritant de " +  super().__str__()
+    
+    def decrire(self):
+        #print(f"Employe prenom={self.prenom} nom={self.nom} age={self.age} salaire={self.salaire} fonction={self.fonction}")
+      
+        super().decrire()
+        print(f"      qui est un employe avec salaire={self.salaire} et fonction={self.fonction}")
+     
+        '''
+        print(f"Employe avec salaire={self.salaire} et fonction={self.fonction}" ,end=" heritant de ")
+        super().decrire()
+        '''
