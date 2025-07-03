b78d93d50a59a8022088205988544f82ae18fd9f
Didier_Defrance <didier@d-defrance.fr>
2025-07-03 11:23:23 +0200

    s

---
+++ tp/tp_bases/e4_Employe.py
@@ -0,0 +1,10 @@
+from e1_Personne import Personne
+
+class Employe(Personne):
+    def __init__(self,prenom="?",nom="?",age =0,salaire=0,fonction="?"):
+        super().__init__(prenom,nom,age) # déclenche indirectement self.prenom=prenom, etc
+        self.salaire=salaire
+        self.fonction=fonction
+
+    def __str__(self):
+        return f"Employe salaire={self.salaire} fonction={self.fonction} heritant de " + super().__str__()
