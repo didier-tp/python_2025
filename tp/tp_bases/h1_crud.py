25d300771f08b36e5fe4070210c77ad519af63d9
Didier_Defrance <didier@d-defrance.fr>
2025-07-04 15:27:19 +0200

    s

--- tp/tp_bases/h1_crud.py
+++ tp/tp_bases/h1_crud.py
@@ -1,9 +1,15 @@
-from sqlalchemy import text
+from sqlalchemy import text,insert
 from h1_db_params import my_db_sql_alchemy_engine,my_db_url
 from h1_metadata import metadata_object , compte_table
 
 
 with my_db_sql_alchemy_engine.begin() as cnx:
-    req_insert_compte="INSERT INTO compte (numero,label,solde) VALUES(1,'comptexy',50.0)"
-    insert_statement=text(req_insert_compte)
-    cnx.execute(insert_statement)
+    #req_insert_compte="INSERT INTO compte (numero,label,solde) VALUES(2,'comptezz',60.0)"
+    #req_insert_compte="INSERT INTO compte (label,solde) VALUES('comptezz',60.0)"
+    #insert_statement=text(req_insert_compte)
+    #res= cnx.execute(insert_statement)
+    
+    insert_stmt = insert(compte_table).values(label='compteXyz', solde=70.0)
+    res=cnx.execute(insert_stmt)
+    auto_incr_pk=res.inserted_primary_key[0] # ici pk avec une seule colonne
+    print("numero du compte ajouté =", auto_incr_pk )
